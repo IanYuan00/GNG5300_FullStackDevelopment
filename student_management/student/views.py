5e79e89b4ba69b8bae0b7a19f7e723229eddadfb
IanYuan00 <yuangao2517@gmail.com>
2024-10-04 00:52:01 -0400

    Pagination

--- student_management/student/views.py
+++ student_management/student/views.py
@@ -2,15 +2,23 @@ from django.shortcuts import render, redirect, get_object_or_404
 from student.models import Student
 from student.forms import StudentForm
 from django.contrib.auth.decorators import login_required
+from django.core.paginator import Paginator
 
 # Create your views here.
 #DIsplay a list of all students
 def student_index(request):
     students = Student.objects.all().order_by('last_name')
-    query = request.GET.get('q')  #Get the search query from the request
-    if query:
-        students = students.filter(first_name__icontains=query)
-    return render(request, 'student/index.html', {'students': students, 'query': query})
+
+    # Set up pagination with 10 students per page
+    paginator = Paginator(students, 10)  # Show 10 students per page
+    
+    # Get the page number from the request (defaults to 1)
+    page_number = request.GET.get('page')
+    
+    # Get the students for the requested page
+    students = paginator.get_page(page_number)
+
+    return render(request, 'student/index.html', {'students': students})
 
 #Display the details of a single student
 def student_detail(request, pk):
